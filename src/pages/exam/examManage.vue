4af4fe1fa9749d383d3763fce1eeac63db18b602
Wesley <985189328@qq.com>
2025-09-03 21:49:18 +0800

    chore: update

---
+++ src/pages/exam/examManage.vue
@@ -0,0 +1,11 @@
+<template>123</template>
+
+<script setup lang="tsx">
+import { defineComponent } from 'vue';
+</script>
+
+<script lang="tsx">
+export default defineComponent({
+  name: 'ExamManage',
+});
+</script>
